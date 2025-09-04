b15dea2eaf76c406690b32d12f19d71c42f399d5
John Doe <johndoe@example.com>
2025-09-04 21:59:26 +0300

    add parametrize for product page tests with xfail on offer7

--- pages/locators.py
+++ pages/locators.py
@@ -6,3 +6,10 @@ class LoginPageLocators:
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class ProductPageLocators():
+    BASKET_BUTTON = (By.CSS_SELECTOR, "button.btn-add-to-basket")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+    MESSAGE_PRODUCT_NAME = (By.CSS_SELECTOR, "#messages .alert-success:nth-child(1) strong")
+    MESSAGE_BASKET_TOTAL = (By.CSS_SELECTOR, ".alert-info .alertinner strong")
---
+++ pages/product_page.py
@@ -0,0 +1,21 @@
+import pytest
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+from .locators import ProductPageLocators
+
+class ProductPage(BasePage):
+    def add_product_to_basket(self):
+        button = self.browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket")
+        button.click()
+
+    def should_be_message_about_adding(self):
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        message_name = self.browser.find_element(*ProductPageLocators.MESSAGE_PRODUCT_NAME).text
+        assert product_name == message_name, \
+            f"Product name in message is wrong. Expected '{product_name}', got '{message_name}'"
+
+    def should_be_message_basket_total(self):
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        basket_total = self.browser.find_element(*ProductPageLocators.MESSAGE_BASKET_TOTAL).text
+        assert product_price == basket_total, \
+            f"Basket total is wrong. Expected '{product_price}', got '{basket_total}'"
